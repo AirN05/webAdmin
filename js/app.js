c487fb1ee203bc53b90590bc8ebda12a01e74473
nastyafilip <nastyafilip@mail.ru>
2016-11-24 20:03:47 +0600

    добавл верстка

--- js/app.js
+++ js/app.js
@@ -63,6 +63,43 @@ app.config(function($routeProvider) {
 })
 
 
+.controller("contentController", function($scope,$http, $log){
+        $scope.sendMessage=function(){
+            // отправка отзыва на почту
+        }
+
+        $http.get('').success(function(data){
+            $scope.contacts=data;
+
+        })
+
+        $http.get('data/employ.json').success(function(data){
+            $scope.employ=data;
+
+        })
+
+        $http.get('data/shop.json').success(function(data){
+            $scope.shop=data;
+
+        })
+
+        $http.get('data/images.json').success(function(data){
+            $scope.images=data;
+
+        })
+
+
+        $http.get('data/price.json').success(function(data){
+            $scope.price=data;
+
+        })
+
+
+
+
+    })
+
+
 
 
 
