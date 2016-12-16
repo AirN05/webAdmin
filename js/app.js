ad85d23282f0cc4035f8a9323c905bf4385ad2c8
AirN05 <nastyafilip@mail.ru>
2016-12-16 10:55:02 +0500

    add auth admin

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ app.config(function($routeProvider) {
 
     })
 
-    .controller("editController", function($scope,$http, $location, $log){  
+    .controller("editController", function($scope,$http, $location, $log, $route){  
 
         $scope.date = new Date();
 
@@ -35,7 +35,7 @@ app.config(function($routeProvider) {
    
   /*for(i=$scope.minDate; i<$scope.maxDate; i++){
     $log.log(i);
-    $scope.days=i;
+    $scope.days=i; html
     $log.log($scope.days);
   }*/
 
@@ -44,26 +44,27 @@ app.config(function($routeProvider) {
     return day === 0 || day === 6;
   };
 
-    $scope.choiceEmp=function(){
+    
 
-        $http.get('data/shedule.json').success(function(data, stdatus, headers, config){
-        $scope.employ = data;
+        $http.get('data/shedule.json').success(function(data, status, headers, config){
+        $scope.shedule = data;
         $log.log($scope.selected);
-        $log.log($scope.employ);
+        $log.log($scope.shedule);
       
             })
-    }
+    
 
-    $scope.change=function(){
+    $scope.changeShedule=function(){
         $http.put('data/shedule.json',  JSON.stringify($scope.selected))
-            .success(function(data, stdatus, headers, config){
+            .success(function(data, status, headers, config){
             $log.log(status);
+            $route.reload();
         })
     }
-})
+
+    
 
 
-.controller("contentController", function($scope,$http, $log){
         $scope.sendMessage=function(){
             // отправка отзыва на почту
         }
@@ -94,15 +95,61 @@ app.config(function($routeProvider) {
 
         })
 
+        $scope.changePrice=function(){
+            $http.put('data/price.json', JSON.stringify($scope.priceSelected))
+            .success(function(data, status, headers, config){
+            $log.log(status);
+            $route.reload();
+        })
+
+        }  
+
+        $scope.addPrice=function(){
+                $log.log("hi");
+                $http.post('data/price.json', {name: $scope.newService, price: $scope.newPrice, option: $scope.newOption})
+                 .success(function(data, status, headers, config){
+                     $log.log(status);
+                     $log.log(data);
+                     $scope.newService=null;
+                     $scope.newPrice=null;
+                     $scope.newOption=null;
+                     
+                })
+            }
+
+        $scope.addEmploy=function(){
+                $log.log("hi");
+                $http.post('data/employ.json', {name: $scope.newName, image: $scope.newPhoto, describe: $scope.newCity})
+                 .success(function(data, status, headers, config){
+                     $log.log(status);
+                     $log.log($scope.newPhoto);
+                     $scope.newName=null;
+                     $scope.newPhoto=null;
+                     $scope.newCity=null;
+                     
+
+                })
+            }
+
+            $scope.changeEmploy=function(){
+                $log.log($scope.employSelected);
+                $http.put('data/employ.json', JSON.stringify($scope.employSelected))
+                    .success(function(data, status, headers, config){
+                    $log.log(status);
+                    $route.reload();
+
+        })
+
+        }  
 
 
 
-    })
+})
 
+   
+// 
 
-
-
-
+    
 .directive("menu", function(){
     return {
         templateUrl:"assets/directives/menu.html",
@@ -116,16 +163,59 @@ app.config(function($routeProvider) {
             .success(function(data, status, headers, config){
                  $scope.menu = data;})  
 
-             $http.get('data/shedule.json').success(function(data, status, headers, config){
+            /* $http.get('data/shedule.json').success(function(data, status, headers, config){
         $scope.employ = data;
         $log.log($scope.selected);
         $log.log($scope.employ);
       
-            })
+            })*/
 
         
 }
 }})
+
+.directive("auth", function(){
+    return {
+        templateUrl:"assets/directives/auth.html",
+        replace: true,
+        restrict: 'E',
+        
+
+            controller: function($scope,$log,$http, $rootScope, $location){
+
+                if (!$rootScope.checkAdmin) $location.path('/page/0');
+
+
+                $scope.auth=function(){
+
+               // $scope.checkAdmin=false;
+                $log.log($scope.login);
+                $log.log($scope.password);
+                $http.post('', {login: $scope.login, password: $scope.password})
+                    .success(function(data){
+                        $log.log("ok");
+
+                    })
+
+                    if ($scope.login=='admin' && $scope.password=='admin'){
+                    $rootScope.checkAdmin=true;
+                    $scope.control=true;
+                    $log.log( $rootScope.checkAdmin);
+                }
+                    //$scope.login=null;
+                   // $scope.password=null;
+            }
+
+            $scope.logout=function(){
+                $rootScope.checkAdmin=false;
+                $log.log("out");
+                $location.path('/index.html');
+
+            }
+
+}
+}})
+
 
 
 
